40ecdabd2d60e6612a8072e82f6df33fd66893c0
guorunfa <guorunfa@aliyun.com>
2021-05-20 10:42:12 +0800

    test

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,3 +1,3 @@
 export default {
-  '@primary-color': 'red'
+  '@primary-color': 'green'
 }
--- vite.config.ts
+++ vite.config.ts
@@ -33,7 +33,17 @@ export default defineConfig({
   //     }
   //   ]
   // },
-
+	server: {
+    port: 3001, // 开发环境启动的端口
+    proxy: {
+      '/api': {
+        // 当遇到 /api 路径时，将其转换成 target 的值
+        target: 'http://47.99.134.126:28019/api/v1',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '') // 将 /api 重写为空
+      }
+    }
+  },
   // antd 按需引入
   plugins: [
     reactRefresh(),
